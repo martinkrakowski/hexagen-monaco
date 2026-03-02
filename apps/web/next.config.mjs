ff76c88dbed07618299ccc12dc543a57f2c0bcd0
Martin Krakowski <martin@nightfall.lan>
2026-03-02 10:02:42 -0500

    chore: Add ShadCN and wrapper components

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,10 +1,19 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
